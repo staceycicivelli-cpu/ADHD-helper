537fb422b46f52878b36a05e2c96723ef870816c
staceycicivelli-cpu <staceycicivelli@gmail.com>
2025-08-16 01:25:32 +1000

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,13 +1,14 @@
+// firebase-messaging-sw.js
 importScripts('https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/12.1.0/firebase-messaging.js');
 
-const firebaseConfig = {
+const firebaseConfig = { 
   apiKey: "AIzaSyA8GxsEaNuijjz1ZGmKJOBkfuAAf6N3czo",
   authDomain: "adhd-easy-mode.firebaseapp.com",
   projectId: "adhd-easy-mode",
-  storageBucket: "adhd-easy-mode.firebasestorage.app",
+  storageBucket: "adhd-easy-mode.appspot.com",
   messagingSenderId: "549461875846",
-  appId: "1:549461875846:web:a671b5438249c0507",
+  appId: "1:549461875846:web:a671b543824fd8439c0507",
   measurementId: "G-2RC70GV6HS"
 };
 
@@ -15,17 +16,13 @@ firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
-// Background message handler
 messaging.onBackgroundMessage(function(payload) {
-  console.log('[firebase-messaging-sw.js] Received background message', payload);
-  const notificationTitle = payload.notification?.title || "Hey friend!";
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
   const notificationOptions = {
-    body: payload.notification?.body || "Need any help?",
-    icon: 'icons/icon-192.png',
-    badge: 'icons/badge-72.png',
-    vibrate: [100, 50, 100]
+    body: payload.notification.body,
+    icon: 'icons/icon-192.png'
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
-
