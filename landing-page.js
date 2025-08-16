0f458b7e947797212d851ac859725d00e073b25e
staceycicivelli-cpu <staceycicivelli@gmail.com>
2025-08-16 11:40:27 +1000

    Update landing-page.js

--- landing-page.js
+++ landing-page.js
@@ -1,7 +1,6 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-import { getMessaging, getToken } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-messaging.js";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js';
+import { getMessaging, getToken } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-messaging.js';
 
-// Your Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyA8GxsEaNuijjz1ZGmKJOBkfuAAf6N3czo",
   authDomain: "adhd-easy-mode.firebaseapp.com",
@@ -12,23 +11,20 @@ const firebaseConfig = {
   measurementId: "G-2RC70GV6HS"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
 
-// Request permission and show token
-async function showToken() {
+async function requestFirebasePermission() {
   const permission = await Notification.requestPermission();
   if (permission === 'granted') {
     const token = await getToken(messaging, { 
       vapidKey: "BNij1cN2k13LMGOOYqGXlBTJO7MyVkIoEik7PBZxpUIngIm3VnOMBEvoVF6Ed48reyq9UOtrT1A2MV96mEeUzK0" 
     });
-    alert("FCM token:\n" + token);
     console.log("FCM token:", token);
+    alert("FCM token generated! Check console to copy it.");
   } else {
     alert("Notifications permission denied");
   }
 }
 
-// Run it on page load
-showToken();
+requestFirebasePermission();
